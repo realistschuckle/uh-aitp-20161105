615e2d0b7c16fd7ed77699a906f15f0fb9201b8a
Curtis Schlak <realistschuckle@gmail.com>
2016-11-06 11:42:04 -0600

    Update with curly brace explanation.

--- docs/js/presentation.js
+++ docs/js/presentation.js
@@ -4,6 +4,9 @@
       src: 'js/highlight.js',
       async: true,
       callback: function() { hljs.initHighlightingOnLoad(); }
-    }]
+    }],
+    width: 1280,
+    height: 720,
+    history: true
   });
 }());
